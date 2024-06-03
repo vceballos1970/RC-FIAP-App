c262d1d298dc8a09b04c4d5ebf254b09be8e0f86
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-06-03 09:21:45 -0500

    Clase para el modelo RegistroWalls

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
+from PyQt5 import QtCore
+
+
 class AcceptanceCriteria:
     def __init__(self, IO_1, LS_1, CP_1, IO_2, LS_2, CP_2):
         self.IO_1 = IO_1
@@ -161,3 +166,82 @@ class BeamElasticElement:
         self.ElegTr = ElegTr
         self.RZi = RZi
         self.RZe = RZe
+
+
+class RegistroWalls:
+    def __init__(self, tbl_data_design_walls, id_, tw, lw, ro_l, ro_t, db, de, Sst, cMaxS, sigma_c, BE):
+        fila = tbl_data_design_walls.rowCount()
+        tbl_data_design_walls.insertRow(fila)
+
+        self.spx_id = QLineEdit(tbl_data_design_walls)
+        self.spx_id.setValidator(QIntValidator(0, 1000))
+        self.spx_id.setText(f'W{id_}')
+
+        self.spx_b = QLineEdit(tbl_data_design_walls)
+        self.spx_b.setValidator(QIntValidator(15, 1000))
+        self.spx_b.setText('{:d}'.format(int(tw)))
+
+        self.spx_h = QLineEdit(tbl_data_design_walls)
+        self.spx_h.setValidator(QIntValidator(20, 2000))
+        self.spx_h.setText('{:d}'.format(int(lw)))
+
+        self.spx_ro_l = QLineEdit(tbl_data_design_walls)
+        self.spx_ro_l.setValidator(QDoubleValidator(0.1, 8., 3))
+        self.spx_ro_l.setText('{:.2f}'.format(ro_l * 100))
+
+        self.spx_ro_t = QLineEdit(tbl_data_design_walls)
+        self.spx_ro_t.setValidator(QDoubleValidator(0.1, 8., 3))
+        self.spx_ro_t.setText('{:.2f}'.format(ro_t * 100))
+
+        self.spx_db = QLineEdit(tbl_data_design_walls)
+        self.spx_db.setValidator(QDoubleValidator(1., 100., 2))
+        self.spx_db.setText('{:.2f}'.format(db))
+
+        self.spx_de = QLineEdit(tbl_data_design_walls)
+        self.spx_de.setValidator(QDoubleValidator(1., 100., 2))
+        self.spx_de.setText('{:.2f}'.format(de))
+
+        self.spx_Sst = QLineEdit(tbl_data_design_walls)
+        self.spx_Sst.setValidator(QIntValidator(2, 100))
+        self.spx_Sst.setText('{:d}'.format(int(Sst)))
+
+        self.spx_cMaxS = QLineEdit(tbl_data_design_walls)
+        # self.spx_cMaxS.setValidator(QDoubleValidator(0.1, 100))
+        self.spx_cMaxS.setAlignment(QtCore.Qt.AlignCenter)
+        self.spx_cMaxS.setText('{:1.3f}'.format(cMaxS))
+
+        self.spx_sigma_c = QLineEdit(tbl_data_design_walls)
+        # self.spx_sigma_c.setValidator(QDoubleValidator(0.01, 100))
+        # self.spx_sigma_c.setAlignment(Qt.AlignHCenter)
+        self.spx_sigma_c.setAlignment(QtCore.Qt.AlignCenter)
+        self.spx_sigma_c.setText('{:1.3f}'.format(sigma_c))
+
+        self.spx_BE = QLineEdit(tbl_data_design_walls)
+        # self.spx_id.setValidator(QIntValidator(0, 1000))
+        self.spx_BE.setText(BE)
+
+        tbl_data_design_walls.setCellWidget(fila, 0, self.spx_id)
+        tbl_data_design_walls.setCellWidget(fila, 1, self.spx_b)
+        tbl_data_design_walls.setCellWidget(fila, 2, self.spx_h)
+        tbl_data_design_walls.setCellWidget(fila, 3, self.spx_ro_l)
+        tbl_data_design_walls.setCellWidget(fila, 4, self.spx_ro_t)
+        tbl_data_design_walls.setCellWidget(fila, 5, self.spx_db)
+        tbl_data_design_walls.setCellWidget(fila, 6, self.spx_de)
+        tbl_data_design_walls.setCellWidget(fila, 7, self.spx_cMaxS)
+        tbl_data_design_walls.setCellWidget(fila, 8, self.spx_sigma_c)
+        tbl_data_design_walls.setCellWidget(fila, 9, self.spx_BE)
+
+        tbl_data_design_walls.setColumnWidth(0, 40)
+        tbl_data_design_walls.setColumnWidth(1, 40)
+        tbl_data_design_walls.setColumnWidth(2, 40)
+        tbl_data_design_walls.setColumnWidth(3, 40)
+        tbl_data_design_walls.setColumnWidth(4, 60)
+        tbl_data_design_walls.setColumnWidth(5, 60)
+        tbl_data_design_walls.setColumnWidth(6, 40)
+        tbl_data_design_walls.setColumnWidth(7, 60)
+        tbl_data_design_walls.setColumnWidth(8, 60)
+        tbl_data_design_walls.setColumnWidth(9, 60)
+
+        stylesheet = "::section{border-style: solid;" \
+                     "border-width: 1px;}"
+        tbl_data_design_walls.horizontalHeader().setStyleSheet(stylesheet)
